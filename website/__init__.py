76709d9bc9edac6f85f8910b2f2a45ac3dae6fd7
hdchong1 <hydchongca@gmail.com>
2022-05-04 08:12:36 -0400

    change user database path

--- website/__init__.py
+++ website/__init__.py
@@ -37,6 +37,6 @@ def create_app():
 
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
+    if not path.exists('/app/website/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
