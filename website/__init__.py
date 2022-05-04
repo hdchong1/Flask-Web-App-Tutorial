1793cbac3f83924892dd9063c8d7d4b542edbb31
hdchong1 <hydchongca@gmail.com>
2022-05-04 08:24:45 -0400

    create database duplication error test

--- website/__init__.py
+++ website/__init__.py
@@ -23,7 +23,7 @@ def create_app():
 
     from .models import User, Note
 
-    create_database(app)
+    # create_database(app)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
